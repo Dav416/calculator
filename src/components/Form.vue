85816020b5c67c4dcf16dc5ab23c2e4b0fc0ea6e
Dav416 <84945215+Dav416@users.noreply.github.com>
2024-11-25 17:59:33 -0500

    feat(Form): added some new logic for area

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,41 +3,71 @@
         <form action="">
             <div class="container">
                 <h1>Calculadora</h1>
-                <label for="selectFigure">
-                    Selecciona una figura
-                </label>
-                <select v-model="selectedFigure" @change=" handleSelectedFigure" name="selectFigure" id="selectFigure">
-                    <option value="" selected disabled>Figuras Planas</option>
-                    <option value="triangle">Triángulo</option>
-                    <option value="rectangle">Rectángulo</option>
-                    <option value="square">Cuadrado</option>
-                    <option value="circle">Círculo</option>
-                    <option disabled>Figuras Tridimensionales</option>
-                    <option value="prisma">Prisma</option>
-                </select>
-                <div v-if="selectedFigure !== ''" class="figuresContainer">
+                <div class="selectGroup">
+                    <label for="selectFigure">
+                        Selecciona una figura
+                    </label>
+                    <select v-model="selectedFigure" @change=" handleSelectedFigure" name="selectFigure" id="selectFigure">
+                        <option value="" selected disabled>Figuras Planas</option>
+                        <option value="triangle">Triángulo</option>
+                        <option value="rectangle">Rectángulo</option>
+                        <option value="square">Cuadrado</option>
+                        <option value="circle">Círculo</option>
+                        <option disabled>Figuras Tridimensionales</option>
+                        <option value="prisma">Prisma</option>
+                    </select>
+                    <label for="selectFigure">
+                        Selecciona un tipo de cálculo
+                    </label>
+                    <select v-model="typeOfCalculation" name="selectTypeOfCalculation" id="selectTypeOfCalculation" @change="handleSelectedTypeOfCalculation">
+                        <option value="" selected disabled> Selecciona una opción </option>
+                        <option value="area">Área</option>
+                        <option value="perimeter">Perímetro</option>
+                        <option value="volume">Volumen</option>
+                    </select>
+                </div>
+                <div v-if="selectedFigure !== '' && typeOfCalculation !== ''" class="figuresContainer">
                     <label>                        
                         {{ figuresLabelText }} {{ selectedFigureText }}
                     </label>
-                    <div class="tripleSideFigures" v-if="selectedFigure === 'triangle'">
-                        <InputFile 
-                            label="Lado 1"
-                            v-model="side1"
-                            id="trianguleSide1"
-                            name="trianguleSide1"
-                        />
-                        <InputFile 
-                            label="Lado 2"
-                            v-model="side2"
-                            id="trianguleSide2"
-                            name="trianguleSide2"
-                        />
-                        <InputFile 
-                            label="Lado 3"
-                            v-model="side3"
-                            id="trianguleSide3"
-                            name="trianguleSide3"
-                        />
+                    <div v-if="selectedFigure === 'triangle'">
+                        <div v-if="typeOfCalculation === 'area'" class="doubleSideFigures">
+                            <InputFile 
+                                label="Base"
+                                v-model="side1"
+                                id="triangleBase" 
+                                name="triangleBase" 
+                            />
+                            <InputFile 
+                                label="Altura"
+                                v-model="side2"
+                                id="triangleHeight"
+                                name="triangleHeight"
+                            />
+                        </div>
+                        <div v-else-if="typeOfCalculation === 'perimeter'" class="tripleSideFigures">
+                            <InputFile 
+                                label="Lado 1"
+                                v-model="side1"
+                                id="trianguleSide1"
+                                name="trianguleSide1"
+                            />
+                            <InputFile 
+                                label="Lado 2"
+                                v-model="side2"
+                                id="trianguleSide2"
+                                name="trianguleSide2"
+                            />
+                            <InputFile 
+                                label="Lado 3"
+                                v-model="side3"
+                                id="trianguleSide3"
+                                name="trianguleSide3"
+                            />
+                        </div>
+                        <div v-else-if="typeOfCalculation === 'volume'">
+                            <p style="color:greenyellow">La opción <strong>Volumen</strong> solo funciona con la figura <strong>Prisma.</strong></p> 
+                        </div>
                     </div>
                     <div class="doubleSideFigures" v-if="selectedFigure === 'rectangle'">
                         <InputFile 
@@ -98,6 +128,11 @@
 </template>
 
 <style scoped>
+    .selectGroup {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
     select {
         padding: 10px 0px;
         margin: 8px 0;
@@ -161,8 +196,10 @@ import { ref, computed } from 'vue'
 import InputFile from './InputFile.vue'
 import confetti from 'canvas-confetti';
 
+const typeOfCalculation = ref('')
 const perimiter = ref(0)
 const volume = ref(0)
+const area = ref(0)
 const side1 = ref(0)
 const side2 = ref(0)
 const side3 = ref(0)
@@ -179,14 +216,22 @@ const showConfetti = () => {
     })
 }
 
+// const figuresLabelText = computed(() => {
+//     if (selectedFigure.value === 'circle') {
+//         return 'Ingrese el radio de la figura'
+//     } else if (selectedFigure.value === 'prisma') {
+//         return 'Ingrese la base y la altura de la figura'
+//     } else {
+//         return 'Ingrese los lados de la figura'
+//     }
+// })
+
 const figuresLabelText = computed(() => {
-    if (selectedFigure.value === 'circle') {
-        return 'Ingrese el radio de la figura'
-    } else if (selectedFigure.value === 'prisma') {
+    if (typeOfCalculation.value === 'area' || selectedFigure.value === 'prisma') {
         return 'Ingrese la base y la altura de la figura'
     } else {
         return 'Ingrese los lados de la figura'
-    }
+    } 
 })
 
 const calculateResultLabel = computed(() => {
@@ -209,9 +254,14 @@ function handleSelectedFigure(event) {
     resetInputs()
 }
 
+function handleSelectedTypeOfCalculation() {
+    resetInputs()
+}
+
 function resetInputs() {
     perimiter.value = 0
     volume.value = 0
+    area.value = 0
     side1.value = 0
     side2.value = 0
     side3.value = 0
