9a609acfc125a64a4658c8d32ab25baccfadfbb3
Dav416 <84945215+Dav416@users.noreply.github.com>
2024-11-25 21:50:37 -0500

    feat(Form): final logic for calculations

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -16,14 +16,20 @@
                         <option disabled>Figuras Tridimensionales</option>
                         <option value="prisma">Prisma</option>
                     </select>
-                    <label for="selectFigure">
+
+                    <label v-if="selectedFigure !== 'prisma'" for="selectFigure">
                         Selecciona un tipo de cálculo
                     </label>
-                    <select v-model="typeOfCalculation" name="selectTypeOfCalculation" id="selectTypeOfCalculation" @change="handleSelectedTypeOfCalculation">
+                    <select 
+                        v-if="selectedFigure !== 'prisma'" 
+                        v-model="typeOfCalculation" 
+                        name="selectTypeOfCalculation" 
+                        id="selectTypeOfCalculation"
+                        @change="handleSelectedTypeOfCalculation"
+                    >
                         <option value="" selected disabled> Selecciona una opción </option>
                         <option value="area">Área</option>
                         <option value="perimeter">Perímetro</option>
-                        <option value="volume">Volumen</option>
                     </select>
                 </div>
                 <div v-if="selectedFigure !== '' && typeOfCalculation !== ''" class="figuresContainer">
@@ -34,13 +40,13 @@
                         <div v-if="typeOfCalculation === 'area'" class="doubleSideFigures">
                             <InputFile 
                                 label="Base"
-                                v-model="side1"
+                                v-model="number1"
                                 id="triangleBase" 
                                 name="triangleBase" 
                             />
                             <InputFile 
                                 label="Altura"
-                                v-model="side2"
+                                v-model="number2"
                                 id="triangleHeight"
                                 name="triangleHeight"
                             />
@@ -48,47 +54,55 @@
                         <div v-else-if="typeOfCalculation === 'perimeter'" class="tripleSideFigures">
                             <InputFile 
                                 label="Lado 1"
-                                v-model="side1"
+                                v-model="number1"
                                 id="trianguleSide1"
                                 name="trianguleSide1"
                             />
                             <InputFile 
                                 label="Lado 2"
-                                v-model="side2"
+                                v-model="number2"
                                 id="trianguleSide2"
                                 name="trianguleSide2"
                             />
                             <InputFile 
                                 label="Lado 3"
-                                v-model="side3"
+                                v-model="number3"
                                 id="trianguleSide3"
                                 name="trianguleSide3"
                             />
                         </div>
-                        <div v-else-if="typeOfCalculation === 'volume'">
-                            <p style="color:greenyellow">La opción <strong>Volumen</strong> solo funciona con la figura <strong>Prisma.</strong></p> 
-                        </div>
                     </div>
                     <div class="doubleSideFigures" v-if="selectedFigure === 'rectangle'">
                         <InputFile 
-                            label="Lado 1 (2L1)"
-                            v-model="side1"
+                            :label="rectangleSide1LabelText"
+                            v-model="number1"
                             id="rectangleSide1"
                             name="rectangleSide1"
                         />
                         <InputFile 
-                            label="Lado 2 (2L2)"
-                            v-model="side2"
+                            :label="rectangleSide2LabelText"
+                            v-model="number2"
                             id="rectangleSide2"
                             name="rectangleSide2"
                         />
                     </div>
-                    <div class="oneSideFigures" v-if="selectedFigure === 'square'">
+                    <div 
+                        :class="typeOfCalculation === 'area' ? 'doubleSideFigures' : 'oneSideFigures'"
+                        v-if="selectedFigure === 'square'"
+                    >
                         <InputFile 
-                            label="Lado (4L)"
-                            v-model="side1"
+                            :label="squareSide1LabelText"
+                            v-model="number1"
                             id="squareSide1"
                             name="squareSide1"
+                        />
+
+                        <InputFile 
+                            v-if="typeOfCalculation === 'area'"
+                            label="Altura"
+                            v-model="number2"
+                            id="squareSide2"
+                            name="squareSide2"
                         />
                     </div>
                     <div class="oneSideFigures" v-if="selectedFigure === 'circle'">
@@ -102,19 +116,19 @@
                     <div class="doubleSideFigures" v-if="selectedFigure === 'prisma'">
                         <InputFile 
                             label="Base"
-                            v-model="side1"
+                            v-model="number1"
                             id="prismaSide1"
                             name="prismaSide1"
                         />
                         <InputFile 
                             label="Altura"
-                            v-model="side2"
+                            v-model="number2"
                             id="prismaSide2"
                             name="prismaSide2"
                         />
                     </div>
                 </div>
-                <div v-if="perimiter !== 0 || volume !== 0">
+                <div v-if="perimiter !== 0 || volume !== 0 || area !== 0">
                     <p>{{ calculateResultLabel }}</p>
                     <p><strong>{{ calculateResultValue }}</strong></p>
                 </div>
@@ -200,9 +214,9 @@ const typeOfCalculation = ref('')
 const perimiter = ref(0)
 const volume = ref(0)
 const area = ref(0)
-const side1 = ref(0)
-const side2 = ref(0)
-const side3 = ref(0)
+const number1 = ref(0)
+const number2 = ref(0)
+const number3 = ref(0)
 const radius = ref(0)
 const pi = Math.PI
 const selectedFigure = ref('')
@@ -210,47 +224,79 @@ const selectedFigureText = ref('')
 
 const showConfetti = () => {
     confetti({
-        particleCount: 100, // Número de partículas
+        particleCount: 500, // Número de partículas
         spread: 200,         // Ángulo de dispersión
         origin: { y: 0.6 }, // Posición del confeti en la pantalla
     })
 }
 
-// const figuresLabelText = computed(() => {
-//     if (selectedFigure.value === 'circle') {
-//         return 'Ingrese el radio de la figura'
-//     } else if (selectedFigure.value === 'prisma') {
-//         return 'Ingrese la base y la altura de la figura'
-//     } else {
-//         return 'Ingrese los lados de la figura'
-//     }
-// })
-
 const figuresLabelText = computed(() => {
-    if (typeOfCalculation.value === 'area' || selectedFigure.value === 'prisma') {
+    if (selectedFigure.value === 'circle') {
+        return 'Ingrese el radio de la figura'
+    }    
+    if (typeOfCalculation.value === 'area' || typeOfCalculation.value === 'volume') {
         return 'Ingrese la base y la altura de la figura'
-    } else {
+    } else if (typeOfCalculation.value === 'perimeter') {
         return 'Ingrese los lados de la figura'
     } 
 })
 
 const calculateResultLabel = computed(() => {
-    if (volume.value !== 0) {
+    if (area.value !== 0) {
+        return `El área de la figura ${selectedFigureText.value} es de `
+    }
+    else if (perimiter.value !== 0) {
+        return `El perímetro de la figura ${selectedFigureText.value} es de `
+    }
+    else if (volume.value !== 0) {
         return `El volumen de la figura ${selectedFigureText.value} es de ` 
     }
-    return `El perímetro de la figura ${selectedFigureText.value} es de `
 })
 
 const calculateResultValue = computed(() => {
     if (volume.value !== 0) {
         return `${volume.value} cm³`
     }
+    else if (area.value !== 0) {
+        return `${area.value} cm²`
+    }
     return `${perimiter.value} cm`
     
 })
 
+const rectangleSide1LabelText = computed(() => {
+    if (typeOfCalculation.value === 'area') {
+        return 'Base'
+    } else if (typeOfCalculation.value === 'perimeter') {
+        return 'Lado 1 (2L1)'
+    } 
+})
+
+const rectangleSide2LabelText = computed(() => {
+    if (typeOfCalculation.value === 'area') {
+        return 'Altura'
+    } else if (typeOfCalculation.value === 'perimeter') {
+        return 'Lado 2 (2L2)'
+    } 
+})
+
+const squareSide1LabelText = computed(() => {
+    if (typeOfCalculation.value === 'area') {
+        return 'Base'
+    } else if (typeOfCalculation.value === 'perimeter') {
+        return 'Lado (4L)'
+    } 
+})
+
 function handleSelectedFigure(event) {
     selectedFigureText.value = event.target.selectedOptions[0].text
+    
+    typeOfCalculation.value = ''
+    
+    if(selectedFigure.value === 'prisma'){
+        typeOfCalculation.value = 'volume'
+    }
+
     resetInputs()
 }
 
@@ -262,9 +308,9 @@ function resetInputs() {
     perimiter.value = 0
     volume.value = 0
     area.value = 0
-    side1.value = 0
-    side2.value = 0
-    side3.value = 0
+    number1.value = 0
+    number2.value = 0
+    number3.value = 0
     radius.value = 0
 }
 
@@ -272,30 +318,46 @@ function Calculate() {
     let calculationCompleted = false
     switch (selectedFigure.value) {
         case 'triangle':
-            perimiter.value = side1.value + side2.value + side3.value
+            if (typeOfCalculation.value === 'area') {
+                area.value = (number1.value * number2.value)  / 2
+            } else if (typeOfCalculation.value === 'perimeter') {
+                perimiter.value = number1.value + number2.value + number3.value
+            }
             calculationCompleted = true
             break;
         case 'rectangle':
-            perimiter.value = 2 * side1.value + 2 * side2.value
+            if (typeOfCalculation.value === 'area') {
+                area.value = number1.value * number2.value
+            } else if (typeOfCalculation.value === 'perimeter') {
+                perimiter.value = 2 * number1.value + 2 * number2.value
+            }
             calculationCompleted = true
             break;
         case 'square':
-            perimiter.value = 4 * side1.value
+            if (typeOfCalculation.value === 'area') {
+                area.value = number1.value * number2.value
+            } else if (typeOfCalculation.value === 'perimeter') {
+                perimiter.value = 4 * number1.value
+            }
             calculationCompleted = true
             break;
         case 'circle':
-            perimiter.value = 2 * pi * radius.value
+            if (typeOfCalculation.value === 'area') {
+                area.value = pi * (radius.value * radius.value) 
+            } else if (typeOfCalculation.value === 'perimeter') {
+                perimiter.value = 2 * pi * radius.value
+            }
             calculationCompleted = true
             break;
         case 'prisma':
-            volume.value = side1.value * side2.value
+            volume.value = number1.value * number2.value
             calculationCompleted = true
             break;
         default:
             console.log('No se seleccionó ninguna figura')
     }
     if (calculationCompleted) {
-        if (perimiter.value !== 0 || volume.value !== 0) {
+        if (area.value !== 0 || perimiter.value !== 0 || volume.value !== 0 ) { 
             showConfetti()
         }
     }
